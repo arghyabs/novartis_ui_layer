ebee84aec19c124cc9b8ced045227eede67d2df2
Arghya Basu <arghyabs@gmail.com>
2019-09-22 10:48:47 +0000

    correcting

--- hello.js
+++ hello.js
@@ -1,10 +1,3 @@
-const hello = () => {
-	return 'Hello World';
+module.exports = function hello () {
+  return 'Hello World!';
 }
-
-module.exports = {
-	hello
-};
-//module.exports = function hello () {
-//  return 'Hello World!';
-//}
